7fbecaa5237495f2f99de2384bc79528e85d5b93
Tom Robinson <tom.robinson@clear.bank>
2023-04-28 13:46:44 +0100

    Initial demo code for type mapping

---
+++ Demos/TypeMapping/Class1.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TypeMapping;
+
+public class Employee
+{
+    public string Name { get; set; }
+    public int Salary { get; set; }
+    public string Address { get; set; }
+    public string Department { get; set; }
+}
+
+public class EmployeeDto
+{
+    public string DisplayName { get; set; }
+    public string Address { get; set; }
+    public string Department { get; set; }
+}
+
+public class Tests
+{
+    [Test]
+    public void ManualMapping()
+    {
+        var employee = new Employee
+        {
+            Name = "Bob",
+            Salary = 20000,
+            Address = "London",
+            Department = "IT"
+        };
+        
+        var employeeDto = new EmployeeDto
+        {
+            DisplayName = employee.Name,
+            Address = employee.Address,
+            Department = employee.Department
+        };
+
+        employeeDto.Should().BeEquivalentTo(employee);
+    }
+    
+    [Test]
+    public void AutoMapperMapping()
+    {
+        var employee = new Employee
+        {
+            Name = "Bob",
+            Salary = 20000,
+            Address = "London",
+            Department = "IT"
+        };
+
+        var mapperConfiguration = new MapperConfiguration(config => config.CreateMap<Employee, EmployeeDto>()
+            .ForMember(m => m.DisplayName, expression => expression.MapFrom(c => c.Name)));
+
+        var mapper = new Mapper(mapperConfiguration);
+
+        var employeeDto = mapper.Map<EmployeeDto>(employee);
+        
+        employeeDto.Should().BeEquivalentTo(employee);
+    }
+}
+
